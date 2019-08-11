026597a98520cdd652e3ae226518c83e9fd84d9c
Tek Loon <tekloon.1991@gmail.com>
2019-08-11 20:07:06 +0800

    Support multiple client subscribing logic

--- src/controllers/quoteController.js
+++ src/controllers/quoteController.js
@@ -6,8 +6,8 @@ const quotesFilePath = path.join(process.cwd(), 'public', 'quotes.json');
 const url = 'http://wisdomquotes.com/stoic-quotes/';
 
 // Quote Event Req
-let quoteEventReq;
-let quoteEventRes;
+let quoteEventReq = [];
+let quoteEventRes = [];
 exports.scrapQuote = () => {
     rp(url).then((html) => {
         let quotes = $('blockquote > p', html);
@@ -30,7 +30,7 @@ exports.scrapQuoteOfTheDay = () => {
     let qotdUrl = 'http://wisdomquotes.com/?time=' + (Date.now());
     rp(qotdUrl).then((html) => {
         let quote = $('.quotescollection-quote', html).text();
-        sendEventToBrowser(quoteEventReq, quoteEventRes, { event: 'newQuote', data: { quote }});
+        sendEventToBrowser(quoteEventRes, { event: 'newQuote', data: { quote }});
         return quote;
     }).catch((err) => {
         console.log('err', err);
@@ -46,16 +46,28 @@ exports.setQuoteEvent = (req, res) => {
         // Without allowing this, you will get Connection Refused
         'Access-Control-Allow-Origin': '*' 
     });
-    quoteEventReq = req;
-    quoteEventRes = res;
-    console.log('Quote Event Setup Successfully', quoteEventRes);
+    req.on('close', () => {
+        res.end();
+    });
+    // Save all the Response Obj to array
+    // So we can inform client when there is new quote
+    quoteEventRes.push(res);
+    console.log('Quote Event Setup Successfully');
 }
 
-const sendEventToBrowser = (req, res, info) => {
-    if (res)  {
-        res.write(`event: ${info.event}\n`);
-        res.write(`data: ${JSON.stringify(info.data)}`);
-        res.write("\n\n");
+const sendEventToBrowser = (resArray, info) => {
+    if (resArray)  {
+        // Remove all closed connection from the array by checking res.finished
+        resArray = resArray.filter((res) => {
+            if(!res.finished) {
+                console.log('update new quote');
+                res.write(`event: ${info.event}\n`);
+                res.write(`data: ${JSON.stringify(info.data)}`);
+                res.write("\n\n");
+                
+            }
+            return (!res.finished);
+        }); 
     } else {
         console.log('Quote Event Res Undefiend');
     }
